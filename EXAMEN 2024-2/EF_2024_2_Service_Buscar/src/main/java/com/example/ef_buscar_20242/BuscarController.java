4e1aef34d9db4d34188b6903311c82fad851590a
Ricardo Alvarado <a20190057@pucp.edu.pe>
2025-07-16 00:55:41 -0500

    kbro el que lea esto

--- EXAMEN 2024-2/EF_2024_2_Service_Buscar/src/main/java/com/example/ef_buscar_20242/BuscarController.java
+++ EXAMEN 2024-2/EF_2024_2_Service_Buscar/src/main/java/com/example/ef_buscar_20242/BuscarController.java
@@ -4,7 +4,6 @@ import com.example.ef_buscar_20242.entity.Employee;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cloud.client.circuitbreaker.CircuitBreakerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,27 +12,34 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-#Completar ...
+@RestController
 public class BuscarController {
 
     @Autowired
     private ServiceBuscarClient serviceBuscarClient;
 
-
-    @GetMapping(value="/...modificar.../{word}/{order}",  produces="application/json")
-    public String listarBuscar(@PathVariable("word") String item_a,
-                                @PathVariable("order") String item_b)
-    { 
+    @GetMapping(value="/rh/filtarempleado/{letra}/{order}", produces="application/json")
+    public String listarBuscar(@PathVariable("letra") String item_a,
+                               @PathVariable("order") String item_b) {
 
         ArrayList<Employee> buscarempleado = new ArrayList<>();
-   
-	// ...
-	// ...
-	// ...
-	// ...
- 
+
+        // Llamar al microservicio EF_Listar usando Feign
+        String response = serviceBuscarClient.listar(item_b);
+
+        // Convertir JSON a lista de empleados usando Gson
+        Gson gson = new Gson();
+        Type listType = new TypeToken<List<Employee>>(){}.getType();
+        List<Employee> empleados = gson.fromJson(response, listType);
+
+        // Filtrar empleados que empiecen con la letra especificada
+        for (Employee emp : empleados) {
+            if (emp.getFirstName() != null &&
+                    emp.getFirstName().toLowerCase().startsWith(item_a.toLowerCase())) {
+                buscarempleado.add(emp);
+            }
+        }
+
         return gson.toJson(buscarempleado);
-
-
     }
 }
--- EXAMEN 2024-2/EF_2024_2_Service_Buscar/src/main/java/com/example/ef_buscar_20242/ServiceBuscarClient.java
+++ EXAMEN 2024-2/EF_2024_2_Service_Buscar/src/main/java/com/example/ef_buscar_20242/ServiceBuscarClient.java
@@ -4,10 +4,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-....(name = ".......")
+@FeignClient(name = "service-employee-hr", url = "http://localhost:8072")
 public interface ServiceBuscarClient {
 
-    @GetMapping("/.... Modificar ...../{ordercolum}")
-    String listar(@PathVariable("ordercolum") String  ordercolum);
+    @GetMapping("/rh/empleado/orderby/{ordercolum}")
+    String listar(@PathVariable("ordercolum") String ordercolum);
 
 }
